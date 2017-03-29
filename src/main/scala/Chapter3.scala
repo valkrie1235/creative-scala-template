6e1453d1618f6cef8d21b59d244a53f1bd6bb6e5
valkrie1235 <ak47an94>
2017-03-29 19:55:32 -0400

    test

--- src/main/scala/Chapter3.scala
+++ src/main/scala/Chapter3.scala
@@ -23,7 +23,8 @@ object Chapter3 {
 
     //Width of a Circle
     val threeCircles = (circle(10) beside circle(10) beside circle(10))
-    val WidthofCircle= (threeCircles on  circle(30)).draw
+    val WidthofCircle= (threeCircles on  circle(30))
+    //WidthofCircle.draw
 
     //EvilEye
     val outerIris = circle(100) fillColor(Color.darkBlue)
@@ -31,8 +32,27 @@ object Chapter3 {
     val pupil = circle(30) fillColor(Color.black)
     val sclera = circle(60) fillColor(Color.white)
     val evilEye = (pupil on innerIris on sclera on outerIris)
-    evilEye.draw
+    //evilEye.draw
 
+    //color pyramid
+    val triangle1 = triangle(60,60) fillColor(Color.darkSlateBlue)
+    val triangle2 = triangle(60,60) fillColor(Color.darkSlateBlue darken 0.2.normalized)
+    val triangle3 = triangle(60,60) fillColor(Color.darkSlateBlue lighten 0.2.normalized)
+    val colorPyramid = triangle1 above (triangle2 beside triangle3)
+    //colorPyramid.draw
+
+    //target
+    val eightRing = circle(40) fillColor(Color.red)
+    val nineRing = circle(25) fillColor(Color.white)
+    val tenRing = circle(10) fillColor(Color.red)
+    val target = (tenRing on nineRing on eightRing)
+
+    val ground = rectangle(100,30) fillColor(Color.green)
+    val stand = rectangle(25,10) fillColor(Color.brown)
+    val pole = rectangle(10,25) fillColor(Color.white)
+
+    val targetStand = (ground below stand below pole below target)
+    //targetStand.draw
 
   }
 
