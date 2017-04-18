c3dc72d091088e60648e61b21b9bff479e48aaf6
valkrie1235 <ak47an94>
2017-04-18 19:51:43 -0400

    Wanted to get started on chapter 8, but missing key images in my printed copy. Will look at .pdf in the office tomorrow.

---
+++ src/main/scala/Chapter8.scala
@@ -0,0 +1,15 @@
+/**
+  * Created by Christopher on 4/18/2017.
+  */
+
+import doodle.core.Color
+import doodle.core._
+import doodle.core.Image._
+import doodle.syntax._
+import doodle.jvm.Java2DCanvas._
+import doodle.backend.StandardInterpreter._
+object Chapter8 {
+
+
+
+}
